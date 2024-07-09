b4c242c0b6962533bb02b8989d87306463ebe9b2
hea97 <s24054@gsm.hs.kr>
2024-07-09 20:12:04 +0900

    push

---
+++ Python/BAEKJOON/9498.py
@@ -0,0 +1,14 @@
+# 24.07.09
+
+score = int(input().strip()) 
+
+if score >= 90:
+    print("A")
+elif score >= 80:
+    print("B")
+elif score >= 70:
+    print("C")
+elif score >= 60:
+    print("D")
+else:
+    print("F")
